b3c23650e14ed3cd63f6a20c78ee49b0dab479af
vice76 <arpityadavaj1999@gmail.com>
2021-10-02 10:32:45 +0530

    about es6 module

---
+++ app.mjs
@@ -0,0 +1,6 @@
+//Acc to es6 module
+import { linearSearch, binarySearch } from "./seach.mjs";
+
+console.log(linearSearch([1, 1, 2, 3, 4], 5));
+
+//iske liye package.json me module system set kr do ya fr .mjs extension use kr lo
---
+++ seach.mjs
@@ -0,0 +1,11 @@
+//Acc to se6 module .mjs is extension
+export const linearSearch = (arr, target) => {
+  for (let element in arr) {
+    if (element == target) return true;
+  }
+  return false;
+};
+
+export const binarySearch = (arr, target) => {
+  return true;
+};
