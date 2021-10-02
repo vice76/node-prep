69c5a7d79136f84bc06049a202e274e51a8ea11a
vice76 <arpityadavaj1999@gmail.com>
2021-10-02 12:25:39 +0530

    about scripting

---
+++ script.js
@@ -0,0 +1,6 @@
+#!/user/bin/env node
+//user ko agar apni file ka access dena ho to use chmod u+x "filename"
+// upr wale ko shaban bolte hai aur scripts k upr likhte hai
+//yeh btaeyega script kaise run hogi
+
+console.log("hello world");
