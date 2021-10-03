04df19c3c18a556483d5754d3b2852b9f79cb3d6
vice76 <arpityadavaj1999@gmail.com>
2021-10-03 09:16:33 +0530

    how to use streams for storing data

--- streams_demo.js
+++ streams_demo.js
@@ -1,11 +1,21 @@
 const fs = require("fs");
+const transformStream = require("stream");
 
 let inputStream = fs.createReadStream(__dirname + "/input.txt");
-
 //console.log(input);
 
 //use piping for transferring data from one place to another in chunks
 //can only pipe from reading mechanisms to wriing mechanisms
 
 let outputStream = process.stdout;
-inputStream.pipe(outputStream);
+
+let targetStream = fs.createWriteStream(__dirname + "/output.txt");
+
+let midStream = new transformStream.Transform({
+  transform(chunk, enc, nextCB) {
+    this.push(chunk.toString().toUpperCase());
+    setTimeout(nextCB, 5000);
+  },
+});
+inputStream.pipe(midStream).pipe(targetStream);
+//for storing data in other files by chunks
