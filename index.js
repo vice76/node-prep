efab02246a2d50d30ee9e204de702ef5213c38bf
vice76 <arpityadavaj1999@gmail.com>
2021-10-08 20:08:08 +0530

    rearranged

--- index.js
+++
@@ -1,4 +0,0 @@
-const action = require("./add");
-// const {} = require('./add'); --yeah destructuring kr do aur jo call krna ho voh mention kr skte hai
-
-console.log(action.subtract(11, 3));
--- package/index.mjs
+++
@@ -1,4 +0,0 @@
-import open from "open";
-
-open("https://www.urbanclap.com");
-//can open tab using this
