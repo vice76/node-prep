af1f8a6bd8fca0e45d11a65a510928913cbbea94
vice76 <arpityadavaj1999@gmail.com>
2021-10-11 22:47:34 +0530

    added custom logger

--- demo_02/Rest_api_express/src/server.js
+++ demo_02/Rest_api_express/src/server.js
@@ -3,11 +3,20 @@ import { json, urlencoded } from "body-parser";
 //hoew to use body-parser using es6 module ,gives error
 
 const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 //console.log("hello");
 
-app.get("/", (req, res) => {
-  res.send({ message: "ok" });
+//custom middleware created
+const customLogger = (req, res, next) => {
+  console.log("Logger incoming");
+  next();
+};
+
+app.get("/", customLogger, (req, res) => {
+  console.log(req.body);
+  res.send({ message: "OK GET" });
 });
 
 app.post("/", (req, res) => {
